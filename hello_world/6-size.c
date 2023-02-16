ed6398cc5d8f4ea6a9e66251e88cc76e21250fbe
Santiago <6317@holbertonstudents.com>
2023-02-16 09:44:23 -0800

    automatic

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,16 @@
  **/
 int main(void)
 {
-printf("Size of char: %ld bytes \n", sizeof(char));
-printf("Size of int: %ld bytes \n", sizeof(int));
-printf("Size of long int: %ld bytes \n", sizeof(long));
-printf("Size of long long int: %ld bytes \n", sizeof(unsigned long));
-printf("Size of float: %ld bytes \n", sizeof(float));
+  int a;
+  long int b;
+  long long c;
+  char d,
+  float e;
+  
+printf("Size of char: %lu byte (s)\n", (unsigned long)sizeof(d));
+printf("Size of int: %lu byte (s)\n", (unsigned long)sizeof(a));
+printf("Size of long int: %lu byte (s)\n", (unsigned long)sizeof(b));
+printf("Size of long long int: %lu byte (s)\n", (unsigned long)sizeof(c));
+printf("Size of float: %lu byte (s)\n", (unsigned long)sizeof(e));
 return (0);
 }
