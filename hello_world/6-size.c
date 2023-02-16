e305bcde38cf1b6d0b4a81faff8e1373799a7fac
Santiago <6317@holbertonstudents.com>
2023-02-16 09:28:15 -0800

    automatic

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
  **/
 int main(void)
 {
-printf("Size of char Data Types in C = %ld bytes \n", sizeof(char));
-printf("Size of int Data Types in C = %ld bytes \n", sizeof(int));
-printf("Size of long int Data Types in C = %ld bytes \n", sizeof(long));
-printf("Size of long long int Data Types in C = %ld bytes \n", sizeof(unsigned long));
-printf("Size of float Data Types in C = %ld bytes \n", sizeof(float));
+printf("Size of char: %ld bytes \n", sizeof(char));
+printf("Size of int: %ld bytes \n", sizeof(int));
+printf("Size of long int: %ld bytes \n", sizeof(long));
+printf("Size of long long int: %ld bytes \n", sizeof(unsigned long));
+printf("Size of float: %ld bytes \n", sizeof(float));
 return (0);
 }
