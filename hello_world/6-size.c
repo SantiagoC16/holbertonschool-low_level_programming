f3ef1b9a46de98aa404c816d4f9d83c8eebf2e3b
Santiago <6317@holbertonstudents.com>
2023-02-16 09:08:27 -0800

    automatic

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+printf("Size of Char Data Types in C = %2d bytes \n", sizeof(char));
+printf("Size of Unsigned char Data Types in C = %2d bytes \n", sizeof(unsigned char));
+ printf("Size of Short Data Types in C = %2d bytes \n", sizeof(short));
+printf("Size of Unsigned short Data Types in C = %2d bytes \n", sizeof(unsigned short));
+printf("Size of Int Data Types in C = %2d bytes \n", sizeof(int));
+printf("Size of Unsigned Int Data Types in C = %2d bytes \n", sizeof(unsigned int));
+printf("Size of Long Int Data Types in C = %2d bytes \n", sizeof(long));
+printf("Size of Unsigned long Data Types in C = %2d bytes \n", sizeof(unsigned long));
+return (0);
+}
