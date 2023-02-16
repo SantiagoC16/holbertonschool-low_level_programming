b9d2b0a7fbb686a54192c7a89595eeaab58848cf
Santiago <6317@holbertonstudents.com>
2023-02-16 08:31:53 -0800

    automatic

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,8 +4,8 @@
  *
  * Return: Always 0 (Success)
  **/
-int main(void)
+int main (void)
 {
-printf("\"Programming is like building a multilingual puzzle");
-return(0);
+printf ("\"Programming is like building a multilingual puzzle");
+return (0);
 }
