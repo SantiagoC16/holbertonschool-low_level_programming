868bbaa01fa5570f75f9749490b257b0497ddd26
Santiago <6317@holbertonstudents.com>
2023-02-21 14:14:06 -0800

    automatic

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -8,7 +8,7 @@
 */
 int main(void)
 {
-  int n;
+int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
@@ -21,6 +21,5 @@ n, n % 10);
 else
 printf("Last digit of %d is %d and is 0\n",
 n, n % 10);
- 
 return (0);
 }
