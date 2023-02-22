8bc6b2a462974c74957f6a42e94f1d83ed333891
Santiago <6317@holbertonstudents.com>
2023-02-22 04:15:53 -0800

    automatic

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - prints all single digit numbers of base 10 starting from 0
  * only with putchar
  * Return: Always 0.
--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
+/**
  * main - Prints the alphabet in lowercase,
  * in reverse.
  *
  * Return: Always 0.
- */
+*/
 
 int main(void)
 {
