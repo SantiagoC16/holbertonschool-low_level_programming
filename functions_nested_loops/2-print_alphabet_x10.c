51f10fa9af4943647ff3d5d491ffb7b85f1e89fd
Santiago <6317@holbertonstudents.com>
2023-02-22 10:17:09 -0800

    automatic

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_alphabet_x10 - Write a function that prints the alphabet, in lowercase,
- * followed by a new line.
+ * print_alphabet_x10 - Write a function that prints the 
+ * alphabet, in lowercase,followed by a new line.
  *
  * Return: Always 0.
  */
@@ -15,6 +15,6 @@ for (i = 0; i < 10; i++)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
-}
 _putchar('\n');
+}
 }
