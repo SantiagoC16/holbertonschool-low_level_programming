ed9fb35429669590b87d959b0801e0f7593d5e06
Santiago <6317@holbertonstudents.com>
2023-02-22 09:38:16 -0800

    automatic

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints _putchar, followed by a new line
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int word;
+printf("_putchar", word);
+return (0);
+}
