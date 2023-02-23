64f08188ef7508901e985ecd6d7d62ef33cb9d1a
Santiago <6317@holbertonstudents.com>
2023-02-23 10:06:03 -0800

    automatic

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@
 void jack_bauer(void)
 {
   int a, b;
-for (a = 0; a < 24; n++)
+for (a = 0; a < 24; a++)
 {
-for (b = 0; b < 60; n++)
+for (b = 0; b < 60; b++)
 {
 _putchar(a /10 + '0');
 _putchar(a % 10 + '0');
