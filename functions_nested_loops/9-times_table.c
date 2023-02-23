d1e0f2a6012c1ed23b2e0486f95050f6d2259a98
Santiago <6317@holbertonstudents.com>
2023-02-23 15:11:21 -0800

    automatic

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - Write a function that prints the 9 times table.
+ * @n: an int.
+ * Return:the last damage
+ */
+void times_table(void)
+{
+int n   
+for (int n = 1; n < 10; n++)
+printf("%d * %d = %d \n", n)
+}
