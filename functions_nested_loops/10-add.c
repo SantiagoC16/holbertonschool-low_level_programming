844e68bd98742bf54d8c898c47a2631a2a9e5fb7
Santiago <6317@holbertonstudents.com>
2023-02-23 17:06:30 -0800

    automatic

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - Write a function that prints the 9 times table.
+ * @n, @i, @a, @b, @c: an int.
+ * Return:the last damage
+ */
+int add(int, int)
+{
+int a, b, c;
+c = a * b;
+_putchar(c, '0')
+}
