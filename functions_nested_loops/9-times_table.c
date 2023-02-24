ab436b01dc84104d46622f849540254bd11135dd
Santiago <6317@holbertonstudents.com>
2023-02-24 04:48:31 -0800

    automatic

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,19 +14,27 @@ for (a = 0; a <= 9; a++)
 for (b = 0; b <= 9; b++)
 {
 c = a * b;
-if (c <= 9)
-{
-_putchar(32);
-_putchar(c + 48);
-}
-else
+if (c > 9)
 {
 _putchar((c / 10) + 48);
 _putchar((c % 10) + 48);
 }
-_putchar(44);
-_putchar(32);
+else if (C != 0)
+{
+_putchar(32 + 48);
+_putchar(c + 48);
+}
+else
+{
+_putchar(c + 48);
+}
+if (b != 9)
+{
+_putchar(44)
+_putchar(32)
+}  
 }
 _putchar('\n');
+}
 }
 }
