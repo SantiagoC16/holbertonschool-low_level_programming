0eaa3a18865369b7c450cb2cac1a3cf1f534c070
Santiago <6317@holbertonstudents.com>
2023-02-24 15:36:02 -0800

    automatic

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,17 +2,19 @@
 #include <stdio.h>
 
 /**
- *print_most_numbers - Write a function that prints 10 times the numbers
- * from 0 to 14
- *Return: Always 0.
+ * more_numbers -  Write a function that prints 10 times
+ * the numbers from 0 to 14
+ * Return: Always 0.
  */
 void more_numbers(void)
 {
 	int c;
+	
 	for (c = 0; c < 15; c++)
 	_putchar(c + '0');
 	{
 		int a;
+
 		for (a = 0; a < 10; a++)
 		_putchar('\n');
 	}
