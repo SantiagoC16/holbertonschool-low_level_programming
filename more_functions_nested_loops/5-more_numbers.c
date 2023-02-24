531fbab4e80d4823162c4770d5022eabf8a60e05
Santiago <6317@holbertonstudents.com>
2023-02-24 15:33:39 -0800

    automatic

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_most_numbers - Write a function that prints 10 times the numbers
+ * from 0 to 14
+ *Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int c;
+	for (c = 0; c < 15; c++)
+	_putchar(c + '0');
+	{
+		int a;
+		for (a = 0; a < 10; a++)
+		_putchar('\n');
+	}
+_putchar('\n');
+}
