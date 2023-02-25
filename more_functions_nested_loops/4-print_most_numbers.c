210ffd5c85b568b688a7397b3325f24eacc3e1db
Santiago <6317@holbertonstudents.com>
2023-02-24 17:21:07 -0800

    automatic

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -4,13 +4,14 @@
 /**
  * print_most_numbers - Write a function that prints the numbers,
  * from 0 to 9, except 2 and 4
- * 
+ *
  * Return: Always 0.
  */
 void print_most_numbers(void)
 {
 	int c;
 	for (c = 0; c < 10; c++)
+
 	{
 		if (c != 2 && c != 4)
 		{
