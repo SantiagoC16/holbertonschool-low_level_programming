c4852fb7b690e2f3c5e22a3dc208dc61a5d64a5c
Santiago <6317@holbertonstudents.com>
2023-02-24 17:20:03 -0800

    automatic

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,13 @@
  */
 void print_most_numbers(void)
 {
-int c;
-for (c = 0; c < 10; c++)
-{
-if (c != 2 && c != 4)
-_putchar (c + '0');
-}
-}
-_putchar('\n');
+	int c;
+	for (c = 0; c < 10; c++)
+	{
+		if (c != 2 && c != 4)
+		{
+		_putchar (c + '0');
+		}
+	}
+	_putchar('\n');
 }
