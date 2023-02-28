5f99872266be5b1fcb2f330a93cd3f4dd9ffaf2a
Santiago <6317@holbertonstudents.com>
2023-02-28 10:02:09 -0800

    ex 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Write a function that prints a string.
+ * @str: is a string
+ * return : always a.
+ */
+
+void _puts(char *str);
+{
+	int a = 0;
+
+	while (*s + a != '\0')
+	{
+	a++;
+	puts(str);
+	}
+	return (a);
+}
