855408c4482626729199aab0c3c13574c1bbda06
Santiago <6317@holbertonstudents.com>
2023-03-02 08:17:35 -0800

    ex 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - Write a function that copies the string pointed to
+ * by src, including the terminating null byte
+ * @dest: is a pointer.
+ * @src: is a pointer.
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int a;
+
+for (a = 0; src[a] != '\0'; a++)
+{
+dest[a] = src[a];
+dest[a] = '\0';
+}
+return (dest);
+}
+
