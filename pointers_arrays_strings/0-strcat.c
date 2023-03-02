4eb84b1a6778c980c687118cd5d9e242b18cb573
Santiago <6317@holbertonstudents.com>
2023-03-02 09:32:47 -0800

    ex 2 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -12,9 +12,9 @@ char *_strcat(char *dest, char *src)
 {
 int a;
 
-int b = strlen(src);
-for (a = 0; dest[a] != '\0'; a++)
-dest[a] = dest[a + b];
+int b = strlen(dest);
+for (a = 0; src[a] != '\0'; a++)
+dest[a] = dest[b + a];
 dest[a] = '\0';
 return (dest);
 }
