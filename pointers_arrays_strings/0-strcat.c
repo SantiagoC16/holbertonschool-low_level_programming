75ec40ed4c9a2be04124e1849f4b73aeafe14e07
Santiago <6317@holbertonstudents.com>
2023-03-02 09:17:47 -0800

    ex 2 1

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - Write a function that concatenates two strings
+ * @dest: is a pointer.
+ * @src: is a pointer.
+ * Return: always
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int a;
+
+int b = strlen(src);
+for (a = 0; dest[a] != '\0'; a++)
+dest[a] = dest[a + b];
+dest[a] = '\0';
+return (dest);
+}
