188be7649a36397d278e8947c719fb7f230a8a1c
Santiago <6317@holbertonstudents.com>
2023-03-03 05:37:34 -0800

    ex 2 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - Write a function that concatenates two strings
+ * @dest: is a pointer.
+ * @src: is a pointer.
+* @n: is an int
+ * Return: always
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int a;
+
+for (a = 0; a < n && src[a] != '\0'; a++)
+dest[a] = src[a];
+dest[a] = '\0';
+return (dest);
+}
