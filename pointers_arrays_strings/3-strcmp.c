e5faa491711cc1eeb9c16b82b12494681cf2ddbe
Santiago <6317@holbertonstudents.com>
2023-03-03 08:25:30 -0800

    ex 2 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,16 +11,10 @@
 int _strcmp(char *s1, char *s2)
 {
 int a;
-int b;
 
-for (a = 0; a != '\0'; a++)
+for (a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
 {
 if (s1[a] != s2[a])
-{
-b = s1[a] - s2[a];
-break;
+return (s1[a] - s2[a]);
 }
 }
-return (b);
-}
-
