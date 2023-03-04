7725ab97506093314b65121aafc4d120ebc17999
Santiago <6317@holbertonstudents.com>
2023-03-04 07:55:24 -0800

    ex 2 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet- Write a function that encodes a string into 1337
+ * @a: is an pointer
+ * Return: always
+ */
+
+char *leet(char *a)
+{
+int b[10] = {65, 69, 79, 84, 76, 97, 101, 111, 116, 108};
+int c[10] = {52, 51, 48, 55, 49, 52, 51, 48, 55, 49};
+int d;
+int e;
+
+for (d = 0; a[d]; d++)
+{
+	for (e = 0; b[e]; e++)
+	{
+	if (a[d] == b[e])
+	{
+		a[d] = c[e];
+	}
+	}
+}
+return (a);
+}
+
