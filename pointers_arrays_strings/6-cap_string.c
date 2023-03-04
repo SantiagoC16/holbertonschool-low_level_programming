653d28b6c13f1a2e6ca2c62b6f6f91aa4b523bb2
Santiago <6317@holbertonstudents.com>
2023-03-04 05:48:28 -0800

    ex 2 67

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - Write a function that capitalizes all words of a string
+ * @a: is an pointer
+ * Return: always
+ */
+
+char *cap_string(char *a)
+{
+int c[13] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 124};
+int b;
+int e;
+
+	for (b = 0; a[b] != '\0';  b++)
+	{
+		for (e = 0; c[e] != '\0';  e--)
+		{
+			if (a[b] >= 97 && a[b] <= 122)
+			{
+				a[b] = a[b] - 32;
+			}
+			if (a[b] >= 97 && a[b] <= 122 && a[0] == a[b])
+			{
+				a[b] = a[b] - 32;
+			}
+		}
+	}
+return (0);
+}
