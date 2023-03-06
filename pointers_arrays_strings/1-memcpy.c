f443ebd6c0e827f2f026337677f0f51a3e5a97d4
Santiago <6317@holbertonstudents.com>
2023-03-06 08:50:16 -0800

    ex 3 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - Write a function that copies memory area
+ * @dest: is an pointer
+ * @src: is a pointer
+ * @n: is an unsigned int
+ * Return: always
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int cont;
+
+for (cont = 0; cont < n; cont++)
+{
+src[cont] = dest[cont];
+}
+return (src);
+}
+
