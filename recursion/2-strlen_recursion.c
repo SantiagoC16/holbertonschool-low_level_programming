705e3bb18b98ceb82c8d2a7a86b4ad90197f7e72
Santiago <6317@holbertonstudents.com>
2023-03-11 14:37:03 -0800

    ex 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -12,11 +12,12 @@
 
 int _strlen_recursion(char *s)
 {
-int a = 0;
+int a;
 
 if (*s != '\0')
 {
-	a = (_strlen_recursion(s + 1) + 1);
+	a = _strlen_recursion(s + 1);
+	return (a++);
 }
 return (0);
 }
