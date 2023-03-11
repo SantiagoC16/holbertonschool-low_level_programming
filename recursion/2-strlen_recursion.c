9351d2b6adbbe2841dd9b413722054c00e8821c4
Santiago <6317@holbertonstudents.com>
2023-03-11 14:28:19 -0800

    ex 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -3,4 +3,21 @@
 
 
 /**
+ * _strlen_recursion - Write a function that returns the length of a string
  *
+ * @s: is a pointer
+ *
+ * Return: always 0
+ */
+
+int _strlen_recursion(char *s)
+{
+int a = 0;
+
+if (*s != '\0')
+{
+	a = _strlen_recursion(s + 1);
+	return (a++);
+}
+return (0);
+}
