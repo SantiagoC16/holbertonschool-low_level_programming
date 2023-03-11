132afe176b71f164014d8b6179b08b8004c09283
Santiago <6317@holbertonstudents.com>
2023-03-11 14:49:22 -0800

    ex 3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -3,4 +3,23 @@
 
 
 /**
+ * factorial - Write a function that returns the factorial of a given number
  *
+ * @n: is an int
+ *
+ * Return: always 0
+ */
+
+int factorial(int n)
+{
+
+if (n < 0)
+{
+	return (-1);
+}
+else if (n == 1)
+{
+	return (1);
+}
+return (n * factorial(n - 1));
+}
