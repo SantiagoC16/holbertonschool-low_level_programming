1d4c5a784c25af1c6c6a977b18fe2e8467529e15
Santiago <6317@holbertonstudents.com>
2023-03-11 15:02:11 -0800

    ex 4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -3,4 +3,24 @@
 
 
 /**
+ * _pow_recursion - Write a function that returns the value of x raised
+ * to the power of y
  *
+ * @x: is an int
+ * @y: is an int
+ *
+ * Return: always 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+	return (-1);
+}
+else if (y == 0)
+{
+	return (1);
+}
+return (x * (_pow_recursion(x, y - 1)));
+}
