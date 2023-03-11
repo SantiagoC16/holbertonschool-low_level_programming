28c88ee2d78e330cf8568b269d08c07804af185c
Santiago <6317@holbertonstudents.com>
2023-03-11 14:11:54 -0800

    ex 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -3,4 +3,18 @@
 
 
 /**
+ * _print_rev_recursion - Write a function that prints a string in reverse
  *
+ * @s: is a pointer
+ *
+ * Return: a
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
+}
