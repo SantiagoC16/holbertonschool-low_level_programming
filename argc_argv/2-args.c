6be20402ee21582021e3ecd331595c0ecb3e2040
Santiago <6317@holbertonstudents.com>
2023-03-12 14:25:04 -0700

    ex 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Write a program that prints all arguments it receive
+ *
+ * @argc: is an int
+ * @a: is
+ *
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__ ((unused)) *a[])
+{
+	int b;
+
+	for (b = 0; b < argc; b++)
+	{
+	printf("%s\n", argc)
+	}
+	return (0);
+}
