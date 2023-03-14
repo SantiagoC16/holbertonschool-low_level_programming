0fa6bdc80cae7bce2a0250e6ca4608c5d33abe1e
Santiago <6317@holbertonstudents.com>
2023-03-14 07:16:06 -0700

    prototypes

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * strdup - Write a function that returns a pointer to a newly allocated space in memory
+ * which contains a copy of the string given as a parameter
+ *
+ * @str: is a pointer
+ *
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+
+
+
+
+
+
+return (0);
+}
---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * str_concat - Write a function that concatenates two strings
+ *
+ * @s1: is a char
+ * @s2: is a char
+ *
+ * Return: 0
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
+
+
+
+
+
+return (0):
+}
