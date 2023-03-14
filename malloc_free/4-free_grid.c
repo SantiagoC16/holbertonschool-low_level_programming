16b587f30df8e03223b88377997d045dbe79b11f
Santiago <6317@holbertonstudents.com>
2023-03-14 07:23:37 -0700

    prototypes

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,3 +1,21 @@
 #include "main.h"
 
+/**
+ * free_grid - Write a function that frees a 2 dimensional grid previously created
+ * by your alloc_grid function
+ *
+ * @grid: is an int
+ * @height: is an int
+ *
+ * Return: 0
+ */
 
+void free_grid(int **grid, int height)
+{
+
+
+
+
+
+
+}
