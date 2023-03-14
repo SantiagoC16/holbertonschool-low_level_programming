a19fa71f801c260b9e60e1173010d83a1e3c3d09
Santiago <6317@holbertonstudents.com>
2023-03-14 07:10:48 -0700

    prototypes

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * create_array - Write a function that creates an array of chars
+ * and initializes it with a specific char
+ *
+ * @size: is an unsigned int
+ * @c: is a char
+ *
+ * Return: 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+
+
+
+
+
+return (0);
+}
