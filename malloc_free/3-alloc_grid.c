6f2526563da343245500af6070a225e71d8adb5e
Santiago <6317@holbertonstudents.com>
2023-03-14 07:21:14 -0700

    protoypes

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Write a function that returns a pointer to a 2 dimensional array of integers
+ *
+ * @width: is an int
+ * @height: is an int
+ *
+ * Return: 0
+ */
+
+int **alloc_grid(int width, int height)
+{
+
+
+
+
+
+
+return (0);
+}
