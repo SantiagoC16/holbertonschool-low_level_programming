217811f08088fab8ab26b7827b27b1d76b3b75e7
Santiago <6317@holbertonstudents.com>
2023-03-19 13:59:14 -0700

    ex 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -22,29 +22,28 @@ if (s1 == NULL || s2 == NULL)
 	s1 = "";
 	s2 = "";
 }
-a = strlen(s1);
-if (n >= strlen(s2))
+for (a = 0; a < s1[a]; a++)
 {
-	n = strlen(s2);
 }
-	else
-	{
-		for (b = 0; b < n; b++)
-		{
-			n = s2[b];
-		}
-	}
-	r = malloc(sizeof(char) * (a + n) + 1);
-	if (r == NULL)
-	{
-		free(r);
-		return (NULL);
-	}
-	else
-	{
+for (b = 0; b < s2[b]; b++)
+{
+}
+if (b >= n)
+{
+	b = n;
+}
+
+r = malloc(sizeof(char) * (a + b) + 1);
+if (r == NULL)
+{
+	free(r);
+	return (NULL);
+}
+else
+{
 	strcpy(r, s1);
 	strcat(r, s2);
-	r = '\0';
-	}
+r = '\0';
+}
 return (r);
 }
