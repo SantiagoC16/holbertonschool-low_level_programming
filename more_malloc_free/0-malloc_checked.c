796c1e13a856d760b97c9cb92dc8ca49445a5b80
Santiago <6317@holbertonstudents.com>
2023-03-19 10:01:33 -0700

    prototypes

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * malloc_checked - write a function that allocates memory using malloc
+ *
+ * @b: is an unsigned int
+ *
+ * Return: 0
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+
+
+
+
+
+
+}
---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_nconcat - write a function that concatenates two strings
+ *
+ * @s1: is a pointer
+ * @s2: is a pointer
+ * @n: is an unsigned int
+ *
+ * Return: 0
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+
+
+
+
+
+
+}
