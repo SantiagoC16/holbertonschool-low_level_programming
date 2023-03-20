0d6d8897d0e92bfb631601ac6f4ce7c5c4b0bf55
Santiago <6317@holbertonstudents.com>
2023-03-20 11:53:03 -0700

    ex 2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _printfilename: print the name of the file it compiles
+ *
+ * Return: 0
+ */
+
+
+
+#ifndef __FILE__
+#define __FILE__
+#endif
+
+void _printfilename(void)
+{
+printf(__FILE__);
+}
