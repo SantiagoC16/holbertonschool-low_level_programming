fd57f9cd2937b37ff2856931168c4202a2024dca
Santiago <6317@holbertonstudents.com>
2023-03-22 10:00:49 -0700

    ex 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+
+/**
+ * print_dog - print the dog struct
+ *
+ *
+ * @d: is a struct
+ *
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+char *namec;
+char *ownerc;
+
+strcpy(namec, dog_t->name);
+strcpy(ownerc, dog_t->owner);
+if (namec == NULL || ownerc == NULL)
+{
+	return (NULL);
+}
+}
