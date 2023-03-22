5ac5b5d04ce7da62ace328a6cf7b250faf1f75fd
Santiago <6317@holbertonstudents.com>
2023-03-22 09:27:16 -0700

    doh

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -24,10 +24,12 @@ typedef struct dog
 	float age;
 	char *name;
 	char *owner;
-}dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 
 #endif
