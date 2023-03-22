4fefd704c7e02f71aa921374736644c82240b372
Santiago <6317@holbertonstudents.com>
2023-03-22 05:31:18 -0700

    ex 1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,17 @@
 #ifndef _dog
 #define _dog
 
+/**
+ * struct dog - an estructure
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ */
+
+
+
 struct dog
 {
 	float age;
