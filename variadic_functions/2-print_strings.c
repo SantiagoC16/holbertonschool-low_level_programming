dd0ea9e199f506712e76e1bf530b542f35202a6b
Santiago <6317@holbertonstudents.com>
2023-03-26 19:06:49 -0700

    ex 3

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,2 +1,39 @@
-#
+#include "variadic_functions.h"
 
+/**
+ * print_strings - Write a function that prints strings, followed by a new line
+ *
+ * @separator: is a const char pointer
+ * @n: is a const unsigned int
+ *
+ * Return: x
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int cont;
+const char *string;
+
+va_start(args, n);
+	if (separator == NULL)
+	{
+		return;
+	}
+	for (cont = 0; n < cont; cont++)
+	{
+	if (string != NULL)
+	{
+	printf("%s\n", va_arg(args, char *));
+	if (cont != (n - 1))
+	{
+		printf("%s", separator);
+	}
+	}
+	if (string == NULL)
+	{
+	printf("(nil)\n");
+	}
+	}
+printf("\n");
+}
