7abda1a4b9a4bd5166cf1007734aeaa0404efc21
Santiago <6317@holbertonstudents.com>
2023-03-26 18:17:43 -0700

    ex 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,1 +1,31 @@
-#
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that print numbers
+ *
+ * @separator: const char pointer?
+ * @n: const unsigned int
+ *
+ *
+ * Return: 0
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int cont;
+
+	va_start(args, n);
+	if (separator == NULL)
+	{
+		return;
+	}
+	for (cont = 0; n < cont; cont++)
+	{
+		printf("%d", va_arg(args, int));
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(args);
+}
