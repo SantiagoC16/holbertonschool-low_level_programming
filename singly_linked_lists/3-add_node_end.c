7ed3b1a7c62f21912cefaaeae6b7d3071a83f6d1
Santiago <6317@holbertonstudents.com>
2023-04-07 17:00:48 -0700

    new functions and some fixes

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+ * recorrer - recorre los nodos hasta llegar al ultimo
+ * @h: pointer to struct
+ * Return: the last node
+ */
+
+size_t recorre(const list_t *h)
+{
+	list_t *recorre = h;
+
+	while (recorre != NULL && recorre->next != NULL)
+	{
+		recorre = recorre->next;
+	}
+	return (recorre);
+}
+
+
+/**
  * add_node_end - adds a new node at the end of a list_t list
  * @str: a pointer
  * @head: pointer to pointer
@@ -11,6 +29,8 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *temp;
 	int lenght = 0;
+
+
 
 	temp = malloc(sizeof(list_t));
 	if (temp == NULL)
