744eba7c685090df0db09e843a8b1e199ea95a28
Santiago <6317@holbertonstudents.com>
2023-04-07 18:55:02 -0700

    3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * add_node_end - adds a new node at the end of a list_t list
@@ -26,7 +27,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 	{
 		*head = last;
-		return last;
+		return (last);
 	}
 	temp = *head;
 	while (temp->next != NULL)
