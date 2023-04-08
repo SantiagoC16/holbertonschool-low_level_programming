63aee3405eabc6d98cb993e41e025160acc73fb9
Santiago <6317@holbertonstudents.com>
2023-04-07 19:13:00 -0700

    task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - Write a function that frees a list_t list
+ * @head: pointer to a struct
+ * Return: o
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp = head;
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(head);
+		head = temp;
+	}
+}
