7e5f73c67cd9a72f9d54a395d6778e9bdf4c9680
Santiago <6317@holbertonstudents.com>
2023-04-09 10:36:27 -0700

    task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free a dlistint_t list
+ * @head: pointer to a struct
+ * Return: adress of the new element
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		free(temp);
+		head = head->prev;
+		free(temp);
+		temp = head;
+	}
+}
