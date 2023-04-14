f829186b0eeefa2b37f405a952648aeed1971d90
Santiago <6317@holbertonstudents.com>
2023-04-14 11:33:44 -0700

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,9 +9,31 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	FILE *c;
 
+	c = fopen(filename, "w");
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 
+	if (text_content != NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-2);
+	}
+	if (filename != NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
 
-
+	fclose(c);
 	return (0);
 }
