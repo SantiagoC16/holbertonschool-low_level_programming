2adc6e76327c55ad16a9cc489d4311c6fc80d71e
Santiago <6317@holbertonstudents.com>
2023-04-14 09:50:55 -0700

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,15 +3,32 @@
 /**
  * read_textfile - a function that read a text file and print it
  * @filename: a pointer of const char
- * @letters: an struct
+ * @letters: int
  * Return: the number of letter of the file
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	FILE *text;
+	char c;
+	size_t letter;
 
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	text = fopen("text", "r");
+	if (text == NULL)
+	{
+		return (0);
+	}
+	c = fgetc(text);
+	while (c != 0 && !feof(text))
+	{
+		c = fgetc(text);
+		letter++;
+	}
 
-
-
-	return (0);
+	fclose(text);
+	return (letter);
 }
