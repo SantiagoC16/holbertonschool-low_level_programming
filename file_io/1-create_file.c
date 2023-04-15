b88f9f63843d5b641f560f9d84c39e4225f1e4ce
Santiago <6317@holbertonstudents.com>
2023-04-15 10:40:14 -0700

    task 1 sintaxis fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		fprintf(filename, "%s", text_content);
+		fprintf(c, "%s", text_content);
 		if (filename != NULL)
 		{
 			return (1);
