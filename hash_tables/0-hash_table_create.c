d2726df036517e5deb6f3bcd32597521de774066
Santiago <6317@holbertonstudents.com>
2023-04-16 09:19:39 -0700

    prototypes

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * hash_table_create - a function that creates a hash table
+ * @size: the size of the array
+ * Return: a pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+
+
+
+
+	return (0);
+}
