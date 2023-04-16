f457b7b8c9155f65f238f6930c5df6610da302b6
Santiago <6317@holbertonstudents.com>
2023-04-16 10:38:19 -0700

    task 5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -8,9 +8,23 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int cont;
+	hash_node_t *temp;
 
-
-
-
-
+	if (ht == NULL)
+	{
+		return;
+	}
+	putchar('{');
+	for (cont = 0; cont < ht->size; cont++)
+	{
+		temp = ht->array[cont];
+		while (temp != NULL)
+		{
+			printf("'%s': '%s'", temp->key, temp->value);
+			temp = temp->next;
+		}
+	}
+	putchar('}');
+	putchar('\n');
 }
