4397b9af6fce1eec2b5eb9076ba1b708b543a1ea
Santiago <6317@holbertonstudents.com>
2023-04-16 08:07:56 -0700

    prototype

---
+++ file_io/3-cp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - a function that copy a file from one place to another
+ * @argc: a
+ * @argv: a
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+
+
+
+
+	return (0);
+}
