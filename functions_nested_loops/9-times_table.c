a96ee92731ae8248fcb96fccd7a04cfb9a104594
Santiago <6317@holbertonstudents.com>
2023-04-22 09:56:19 -0700

    taskdf9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,23 +15,19 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			if (c < 10)
+			if (c >= 10)
 			{
-				if (c == 0)
-				{
-					_putchar(c + '0');
-					_putchar(' ');
-				}
-				else
-				{
-				_putchar(' ');
-				_putchar(c + '0');
-				}
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+			}
+			else if (b != 0)
+			{
+			_putchar(' ');
+			_putchar(c + '0');
 			}
 			else
 			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
+				_putchar(c + '0');
 			}
 			if (b != 9)
 			{
