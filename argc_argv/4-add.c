a0c0aa7205ad2d643226c83f4c6f1326fd82ae29
Santiago <6317@holbertonstudents.com>
2023-04-22 10:54:18 -0700

    task4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -4,27 +4,27 @@
  * main - Write
  *
  * @argc: is an int
- * @a: is
+ * @argv: is and array
  *
  * Return: 0
  */
 
-int main(int argc, char *a[])
+int main(int argc, char *argv[])
 {
 	int i, j;
 	int s = 0;
 
 	for (i = 0; i < argc; i++)
 	{
-		for (j = 0; a[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (a[i][j] < 48 || a[i][j] > 57)
+			if (argv[i][j] < 48 || argv[i][j] > 57)
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		s += atoi(a[i]);
+		s += atoi(argv[i]);
 	}
 	printf("%d\n", s);
 	return (0);
