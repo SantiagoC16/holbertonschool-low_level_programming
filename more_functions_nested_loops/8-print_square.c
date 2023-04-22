753fa8cfc0c499e0b68989b353daa97a10e7325b
Santiago <6317@holbertonstudents.com>
2023-04-22 08:18:03 -0700

    task 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,7 +8,7 @@
 
 void print_square(int size)
 {
-	int i, j, k = 0;
+	int i, j;
 
 	if (size <= 0)
 	{
@@ -18,13 +18,11 @@ void print_square(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
-			for (j = 0; j < k; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
 			_putchar('\n');
-			k++;
 		}
 	}
 }
