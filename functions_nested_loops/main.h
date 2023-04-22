5aa1aecb4923d5c5c91c1144e5fa9dbc87539980
Santiago <6317@holbertonstudents.com>
2023-04-22 10:11:26 -0700

    task 11

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _putchar(char c);
@@ -9,3 +13,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_to_98(int n);
