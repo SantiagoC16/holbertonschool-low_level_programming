c04fa1cd01ac6a9dd8e8e7b09073e24d74c08873
Santiago <6317@holbertonstudents.com>
2023-04-22 07:38:38 -0700

    task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - a function that draws a straight line in the terminal
+ * @n: int
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+			_putchar('\n');
+		}
+	}
+}
