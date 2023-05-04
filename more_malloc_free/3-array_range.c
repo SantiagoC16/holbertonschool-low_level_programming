c1376cae74f16cd8b4cdabc330ebc10b8e1ca23d
Santiago <6317@holbertonstudents.com>
2023-05-04 15:20:31 -0700

    last task

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -11,11 +11,14 @@
 
 int *array_range(int min, int max)
 {
+	int *p, i, j;
 
-
-
-
-
-
-
+	if (min > max)
+		return (NULL);
+	p = malloc(sizeof(int) * ((max - min) + 1));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0, j = min; j <= max; j++, i++)
+		p[i] = j;
+	return (p);
 }
