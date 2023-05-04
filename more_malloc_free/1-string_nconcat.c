8152f61a40152998dbc83086e449b84d6bec78ec
Santiago <6317@holbertonstudents.com>
2023-05-04 14:51:39 -0700

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,38 +12,25 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int a;
-unsigned int b;
 char *r;
 
 
-if (s1 == NULL || s2 == NULL)
-{
+if (s1 == NULL)
 	s1 = "";
+if (s2 == NULL)
 	s2 = "";
-}
-for (a = 0; s1[a]; a++)
-{
-}
-for (b = 0; s2[b]; b++)
-{
-}
-if (b >= n)
-{
-	b = n;
-}
-
-r = malloc(sizeof(char) * (a + n) + 1);
+if (n >= strlen(s2))
+	n = strlen(s2);
+r = malloc(sizeof(char) * (strlen(s1) + n) + 1);
 if (r == NULL)
 {
-	free(r);
 	return (NULL);
 }
 else
 {
 	strcpy(r, s1);
 	strcat(r, s2);
-r = '\0';
+	r = '\0';
 }
 return (r);
 }
