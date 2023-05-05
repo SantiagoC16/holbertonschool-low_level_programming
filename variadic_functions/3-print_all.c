16e18bb1490cf4a7782c7140925b495b56ba0dbf
SantiagoC16 <6317@holbertonstudents.com>
2023-05-05 10:49:48 -0700

    task 4

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,6 +1,61 @@
 #include "variadic_functions.h"
 
 /**
+ * characters - print chars
+ * @ex4: va_list
+ * Return: 0
+ */
+
+void characters(va_list ex4)
+{
+	char c;
+
+	c = va_arg(ex4, int);
+	printf("%c", c);
+}
+
+/**
+ * floatx - print float numbs
+ * @ex4: va_list
+ * Return: 0
+ */
+void floatx(va_list ex4);
+{
+	double f;
+
+	f = va_arg(ex4, double);
+	printf("%f", f);
+}
+
+/**
+ * strings - print strings
+ * @ex4: va_list
+ * Return: 0
+ */
+
+void strings(va_list ex4);
+{
+	char *s;
+
+	s = va_arg(ex4, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
+/**
+ * numbers - print numbers
+ * @ex4: va_list
+ * Return: 0
+ */
+void numbers(va_list ex4);
+{
+	int i;
+
+	i = va_arg(ex4, int);
+	printf("%d", i);
+}
+
+/**
  * print_all - Write a function that prints anything
  *
  * @format: char
@@ -9,30 +64,35 @@
  * Return: 0
  */
 
-void print_all(const char * const format, ...)
+void print_all(const char *const format, ...)
 {
-va_list ex4;
-int cont = 0;
-int ints;
-char chars;
-float floats;
-char *str;
+	va_list ex4;
+	int cont = 0, cont2 = 0;
+	char *sep = "", *sep2 = ", ";
+	print_t keys[] = {
+		{"c", characters},
+		{"s", strings},
+		{"i", numbers},
+		{"f", floatx},
+		{NULL, NULL},
+	};
 
-va_start(ex4, format);
-while (format != NULL && format[cont] != '\0')
-{
+	va_start(ex4, format);
+	while (format != NULL && format[cont] != '\0')
+	{
+		while(keys[cont2].f)
+		{
+			if (format[cont2] == keys[cont2].f)
+			{
+				printf("%s", sep);
+				keys[cont2].f(ex4);
+				sep = sep2;
+			}
+			cont2++;
+		}
+		cont++;
+	}
 
-
-	s = va_arg(ex4, char *);
-	f = va_arg(ex4, double);
-	i = va_arg(ex4, int);
-	c = va_arg(ex4, int);
-cont++;
-}
-
-
-
-
-printf("\n");
-va_end(ex4)
+	printf("\n");
+	va_end(ex4);
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,9 +8,28 @@
 #include <math.h>
 #include <stdarg.h>
 
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct print - struct
+ * @type: c
+ * @f: function
+ */
+
+typedef struct print
+{
+	char *type;
+	int (*f)(va_list types);
+} print_t;
+
+
+void characters(va_list ex4);
+void floats(va_list ex4);
+void strings(va_list ex4);
+void numbers(va_list ex4);
 
 #endif
