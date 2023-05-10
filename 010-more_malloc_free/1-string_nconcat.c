fb7700baba7363ee7a4cc344662d55ab12d2d067
SantiagoC16 <6317@holbertonstudents.com>
2023-05-09 18:34:40 -0700

    rename folder

--- 010-more_malloc_free/1-string_nconcat.c
+++ 010-more_malloc_free/1-string_nconcat.c
@@ -12,33 +12,32 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int a;
-unsigned int b;
-char *r;
+	unsigned int a;
+	unsigned int b;
+	char *r;
 
-
-if (s1 == NULL)
-	s1 = "";
-if (s2 == NULL)
-	s2 = "";
-if (n >= strlen(s2))
-	n = strlen(s2);
-r = malloc(sizeof(char) * (strlen(s1) + n) + 1);
-if (r == NULL)
-{
-	return (NULL);
-}
-else
-{
-	for (a = 0; s1[a]; a++)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (n >= strlen(s2))
+		n = strlen(s2);
+	r = malloc(sizeof(char) * (strlen(s1) + n) + 1);
+	if (r == NULL)
 	{
-		r[a] = s1[a];
+		return (NULL);
 	}
-	for (b = 0; b < n; b++, a++)
+	else
 	{
-		r[a] = s2[b];
+		for (a = 0; s1[a]; a++)
+		{
+			r[a] = s1[a];
+		}
+		for (b = 0; b < n; b++, a++)
+		{
+			r[a] = s2[b];
+		}
+		r[a] = '\0';
 	}
-	r[a] = '\0';
-}
-return (r);
+	return (r);
 }
