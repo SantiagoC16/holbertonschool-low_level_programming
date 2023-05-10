7c630fd84b11485e96c739afaca79cf7ee17610c
SantiagoC16 <6317@holbertonstudents.com>
2023-05-09 19:15:27 -0700

    rename folder

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 
-
 /**
  * new_dog - Write a function that creates a new dog
  *
@@ -13,20 +12,24 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-char *namec;
-char *ownerc;
+	char *namec;
+	char *ownerc;
 
-for (namec = 0; namec < name; namec++)
-{ ; }
-for (ownerc = 0; ownerc < owner; ownerc++)
-{ ; }
-namec = malloc(sizeof(char) * (strlen(name) + 1));
-ownerc = malloc(sizeof(char) * (strlen(owner) + 1));
-strcpy(namec, dog_t->name);
-strcpy(ownerc, dog_t->owner);
-if (namec == NULL || ownerc == NULL)
-{
-	return (NULL);
-}
-return (dog_t);
+	for (namec = 0; namec < name; namec++)
+	{
+		;
+	}
+	for (ownerc = 0; ownerc < owner; ownerc++)
+	{
+		;
+	}
+	namec = malloc(sizeof(char) * (strlen(name) + 1));
+	ownerc = malloc(sizeof(char) * (strlen(owner) + 1));
+	strcpy(namec, dog_t->name);
+	strcpy(ownerc, dog_t->owner);
+	if (namec == NULL || ownerc == NULL)
+	{
+		return (NULL);
+	}
+	return (dog_t);
 }
