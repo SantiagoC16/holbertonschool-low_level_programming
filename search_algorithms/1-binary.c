8b4300ffa455faa1d3e73650232565f3656d2bc4
Santiago Caritat <6317@holbertonstudents.com>
2023-05-25 08:18:02 -0700

    prototypes functions

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - a function that searches for a value in a sorted
+ * array of integers using the Binary search algorithm
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return:  the first index where value is located
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	return ();
+}
