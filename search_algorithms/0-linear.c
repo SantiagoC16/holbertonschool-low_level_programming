7ba836c07d8def430341d8ec0fc40a838d957fbc
SantiagoC16 <6317@holbertonstudents.com>
2023-05-27 11:19:19 -0700

    task 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -21,7 +21,7 @@ int linear_search(int *array, size_t size, int value)
 		if (array[search] == value)
 		{
 			printf("Found %d at index: %u", value, array[search]);
-			return (EXIT_SUCCESS);
+			return (0);
 		}
 	}
 }
