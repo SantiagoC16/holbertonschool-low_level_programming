518b19502d12cd6cd8679b4e40b294bc8997322b
SantiagoC16 <6317@holbertonstudents.com>
2023-05-27 11:44:03 -0700

    task 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -19,7 +19,7 @@ int linear_search(int *array, size_t size, int value)
 	for (search = 0; search < size; search++)
 	{
 		printf("Value checked array[%ld] = [%u]\n", search, array[search]);
-		if (array[search] == value)
+		if (value == array[search] )
 		{
 			return (search);
 		}
