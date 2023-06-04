dc79e5d58f7419c56124deba1fe3f42b614e8be3
SantiagoC16 <6317@holbertonstudents.com>
2023-06-04 16:41:07 -0700

    rename folder

--- 05-pointers_arrays_strings/5-strstr.c
+++ 05-pointers_arrays_strings/5-strstr.c
@@ -13,19 +13,22 @@ char *_strstr(char *haystack, char *needle)
 	int a;
 	int b;
 	int c = 0;
+	int d = 0;
 
 	if (needle[0] == '\0')
-		return (haystack);
+		return (haystack + 0);
 	for (a = 0; haystack[a] != '\0'; a++)
 	{
 		c = a;
 		for (b = 0; needle[b] == haystack[c]; b++, c++)
 		{
+			if (d == 0)
+				d = c;
 			if (needle[b] == '\0')
-			{
-				return (&haystack[a]);
-			}
+				return (haystack + a);
 		}
+		b = 0;
+		d = 0;
 	}
 	return (NULL);
 }
